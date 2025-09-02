6934a16481948b187545fd4f1ee3799b3522c564
EugeneWay <relax211@gmail.com>
2025-09-02 17:55:48 +0200

    delete build

--- relayer/dist/types.js
+++
@@ -1,8 +0,0 @@
-export const ETH_PINGER_ABI = [
-    {
-        anonymous: false,
-        inputs: [{ indexed: true, internalType: 'address', name: 'from', type: 'address' }],
-        name: 'PingFromEthereum',
-        type: 'event',
-    },
-];
